174d9fa1535029abbef6efbc938ba503312db676
ElBrokken <6372620+lizberockin@users.noreply.github.com>
2024-11-25 19:51:43 -0800

    Updating how we call urls, write local json file and read in info

--- requests.py
+++ requests.py
@@ -29,9 +29,9 @@ def global_access(host="8.8.8.8", port=53, timeout=3):
 def api_request(address, pw_hash):
     if not hasattr(api_request, "initial_connection"):
         api_request.initial_connection = True
-    max_attempts = 100 if api_request.initial_connection else 10
+    max_attempts = 10 if api_request.initial_connection else 10
     attempts = 0
-    query = "?summary&overTimeData10mins&getQueryTypes&getQuerySources&auth=%s" % pw_hash
+    # query = "?summary&overTimeData10mins&getQueryTypes&getQuerySources&auth=%s" % pw_hash
 
     #retrieve and decode json data from server
     while True:
@@ -40,9 +40,10 @@ def api_request(address, pw_hash):
                 if os.geteuid() == 0:
                     config.LOGGER.info('Initiating connection with server.')
                 print('Initiating connection with server.')
-            with urllib.request.urlopen("http://%s/admin/api.php%s" % (address, query)) as url:
+            os.system("pihole -c -j > test.json")
+            with open("test.json", 'r') as file:
+                raw_data = json.loads(file.read())
                 attempts += 1
-                raw_data = json.loads(url.read().decode())
                 break
         except json.decoder.JSONDecodeError:
             if attempts < max_attempts:
@@ -64,7 +65,7 @@ def api_request(address, pw_hash):
             else:
                 sys.exit(1)
 
-    if 'domains_over_time' not in raw_data or 'ads_over_time' not in raw_data or \
+    if 'domains_being_blocked' not in raw_data or 'ads_blocked_today' not in raw_data or \
        'ads_percentage_today' not in raw_data:
         if os.geteuid() == 0:
             config.LOGGER.error('Invalid data returned from server. Check if pihole-FTL service is \
